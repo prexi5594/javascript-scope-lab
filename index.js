632be7df6d868492513dbb5654df1726478fad3e
Prexidazie Morara <moraraprexidazie@gmail.com>
2025-12-18 12:01:07 +0300

    Final fix: exact if(true)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addBurger() {
   burgers.push(newBurger);
 
   // Block scope
-  if (true) {
+  if(true) {
     const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
   }
